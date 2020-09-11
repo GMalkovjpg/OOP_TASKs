ab4be94abd830d8625316d591f7be519a8d3a8f0
GMalkovjpg <71029820+GMalkovjpg@users.noreply.github.com>
2020-09-11 15:12:46 +0300

    ООП_Семинар1_Мальков2071

---
+++ RAT/RAT/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RAT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        public void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        public void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = "-";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label1.Text = "*";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            label1.Text = "/";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            switch (label1.Text)
+            {
+                case "+" :
+                    int ch1 = Convert.ToInt32(textBox1.Text);
+                    int zn1 = Convert.ToInt32(textBox2.Text);
+                    int ch2 = Convert.ToInt32(textBox3.Text);
+                    int zn2 = Convert.ToInt32(textBox4.Text);
+                    Rational c = new Rational(ch1, zn1);
+                    Rational z = new Rational(ch2, zn2);
+                    Rational itog = c + z;
+                    textBox5.Text = Convert.ToString(itog.numer);
+                    textBox6.Text = Convert.ToString(itog.denumer);
+
+
+                    break;
+                case "-":
+                     ch1 = Convert.ToInt32(textBox1.Text);
+                     zn1 = Convert.ToInt32(textBox2.Text);
+                     ch2 = Convert.ToInt32(textBox3.Text);
+                     zn2 = Convert.ToInt32(textBox4.Text);
+                     c = new Rational(ch1, zn1);
+                     z = new Rational(ch2, zn2);
+                     itog = c - z;
+                    textBox5.Text = Convert.ToString(itog.numer);
+                    textBox6.Text = Convert.ToString(itog.denumer);
+
+                    break;
+                case "*":
+                     ch1 = Convert.ToInt32(textBox1.Text);
+                     zn1 = Convert.ToInt32(textBox2.Text);
+                     ch2 = Convert.ToInt32(textBox3.Text);
+                     zn2 = Convert.ToInt32(textBox4.Text);
+                     c = new Rational(ch1, zn1);
+                     z = new Rational(ch2, zn2);
+                     itog = c * z;
+                    textBox5.Text = Convert.ToString(itog.numer);
+                    textBox6.Text = Convert.ToString(itog.denumer);
+
+                    break;
+                case "/":
+                     ch1 = Convert.ToInt32(textBox1.Text);
+                     zn1 = Convert.ToInt32(textBox2.Text);
+                     ch2 = Convert.ToInt32(textBox3.Text);
+                     zn2 = Convert.ToInt32(textBox4.Text);
+                     c = new Rational(ch1, zn1);
+                     z = new Rational(ch2, zn2);
+                     itog = c / z;
+                    textBox5.Text = Convert.ToString(itog.numer);
+                    textBox6.Text = Convert.ToString(itog.denumer);
+
+                    break;
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RAT/RAT/Rational.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RAT
+{
+    class Rational
+    {
+        public int numer;
+        public int denumer;
+
+        public Rational(int numer, int denumer)
+        {
+            this.numer = numer;
+            this.denumer = denumer;
+        }
+
+        public Rational Multyply(Rational second)
+        {
+            Rational res = new Rational(second.numer, second.denumer);
+            res.numer = this.numer * second.numer;
+            res.denumer = this.denumer * second.denumer;
+
+            return res;
+        }
+        public Rational Divide(Rational second)
+        {
+            Rational res = new Rational(second.numer, second.denumer);
+            res.numer = this.numer / second.numer;
+            res.denumer = this.denumer / second.denumer;
+
+            return res;
+        }
+        public Rational Sum(Rational second)
+        {
+            Rational res = new Rational(second.numer, second.denumer);
+            if (this.denumer == second.denumer)
+            {
+                res.numer = this.numer + second.numer;
+                res.denumer = this.denumer;
+            }
+            if (this.denumer != second.denumer)
+            {
+                res.numer = this.numer * second.denumer + second.numer * this.denumer;
+                res.denumer = this.denumer * second.denumer;
+            }
+            return res;
+        }
+        public Rational Razn(Rational second)
+        {
+            Rational res = new Rational(second.numer, second.denumer);
+            if (this.denumer == second.denumer)
+            {
+                res.numer = this.numer - second.numer;
+                res.denumer = this.denumer;
+            }
+            if (this.denumer != second.denumer)
+            {
+                res.numer = this.numer * second.denumer - second.numer * this.denumer;
+                res.denumer = this.denumer * second.denumer;
+            }
+            return res;
+        }
+        public static Rational operator +(Rational c, Rational z)
+        {
+
+            return c.Sum(z);
+        }
+        public static Rational operator -(Rational c, Rational z)
+        {
+
+            return c.Razn(z);
+        }
+        public static Rational operator *(Rational c, Rational z)
+        {
+
+            return c.Multyply(z);
+        }
+        public static Rational operator /(Rational c, Rational z)
+        {
+
+            return c.Divide(z);
+        }
+    }
+}
+
